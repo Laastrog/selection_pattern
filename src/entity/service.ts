0961a78ec223cf74c118da5c054bc6d0059d77c3
Laastrog <laastrog@yandex.ru>
2025-07-28 20:12:46 +0300

    [TypeORM] Смотреть конфиг

---
+++ src/entity/service.ts
@@ -0,0 +1,11 @@
+import {Entity, Column, PrimaryGeneratedColumn} from "typeorm"
+
+@Entity("services")
+export class Service{
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    name: string
+    shortText: string
+}
